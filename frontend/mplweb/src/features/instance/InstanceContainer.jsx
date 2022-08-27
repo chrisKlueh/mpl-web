d2b8bba0901070c036eb39bc37e269287c7f7e12
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2022-08-27 15:28:35 +0200

    Add Navbar to InstanceContainer, prepare InstanceContainer

--- frontend/mplweb/src/features/instance/InstanceContainer.jsx
+++ frontend/mplweb/src/features/instance/InstanceContainer.jsx
@@ -1,6 +1,36 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-export default function InstanceContainer(props) {
-  const { id } = useParams();
-  return <div>InstanceContainer for instance {id}</div>;
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import WrapperContainer from "../general/WrapperContainer";
+// import { showDemoDetailsRequest } from "../../slices/demoDetailsSlice";
+
+class InstanceContainer extends Component {
+  componentDidMount() {
+    // this.handleShowDemoDetails();
+  }
+
+  handleShowDemoDetails = () => {
+    // const { showDemoDetailsRequest } = this.props;
+    // showDemoDetailsRequest();
+  };
+
+  render() {
+    // const { isLoading, details } = this.props;
+    return <WrapperContainer pageTitle="InstanceContainer"></WrapperContainer>;
+  }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    // details: state.demodetails.details,
+    // isLoading: state.demodetails.isGettingDemoDetails,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // showDemoDetailsRequest: () => dispatch(showDemoDetailsRequest()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(InstanceContainer);
