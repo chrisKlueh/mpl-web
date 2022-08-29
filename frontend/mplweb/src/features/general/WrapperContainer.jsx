d9a8765fd3e10f9f716743640b0ff8684d7d354e
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2022-08-29 14:08:51 +0200

    Style WrapperContainer

--- frontend/mplweb/src/features/general/WrapperContainer.jsx
+++ frontend/mplweb/src/features/general/WrapperContainer.jsx
@@ -2,8 +2,27 @@ import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { Paper, Typography } from "@mui/material";
+import { styled } from "@mui/system";
 
 import Navbar from "./Navbar";
+
+const StyledPaper = styled(Paper)(() => ({
+  width: "75%",
+  height: "calc(100vh - 300px)",
+  margin: "96px auto",
+  paddingTop: "5px",
+}));
+
+const StyledTypography = styled(Typography)(() => ({
+  margin: "0 0 0 10px",
+}));
+
+const StyledChildren = styled("div")(() => ({
+  display: "flex",
+  flexDirection: "column",
+  height: "calc(100vh - 382px)",
+  justifyContent: "space-between",
+}));
 
 const WrapperContainer = (props) => {
   const { isLoggedIn, pageTitle } = props;
@@ -11,12 +30,17 @@ const WrapperContainer = (props) => {
   return isLoggedIn ? (
     <Fragment>
       <Navbar isLoggedIn={isLoggedIn} />
-      <Paper>
-        <Typography variant={"h5"} component={"h5"} gutterBottom align="left">
+      <StyledPaper>
+        <StyledTypography
+          variant={"h5"}
+          component={"h5"}
+          gutterBottom
+          align="left"
+        >
           {pageTitle}
-        </Typography>
-        <div>{props.children}</div>
-      </Paper>
+        </StyledTypography>
+        <StyledChildren>{props.children}</StyledChildren>
+      </StyledPaper>
     </Fragment>
   ) : (
     <Navigate to={{ pathname: "/login" }} />
