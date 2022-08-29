e5354ae8b0facbd3dec02c06924948173295f2f1
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2022-08-29 15:09:51 +0200

    Move Navbar's styling to module

--- frontend/mplweb/src/features/general/Navbar.jsx
+++ frontend/mplweb/src/features/general/Navbar.jsx
@@ -1,35 +1,27 @@
 import React, { useState } from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-// import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-// import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Avatar,
+  Tooltip,
+  MenuItem,
+} from "@mui/material";
 import TimelineIcon from "@mui/icons-material/Timeline";
-
 import { Link } from "react-router-dom";
 
-// const pages = ["Products", "Pricing", "Blog"];
+import styles from "./Navbar.module.css";
+
 const Navbar = (props) => {
-  //   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
 
-  //   const handleOpenNavMenu = (event) => {
-  //     setAnchorElNav(event.currentTarget);
-  //   };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
-
-  //   const handleCloseNavMenu = () => {
-  //     setAnchorElNav(null);
-  //   };
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
@@ -45,105 +37,30 @@ const Navbar = (props) => {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <TimelineIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <TimelineIcon className={styles.logo} />
           <Link to="/demos" style={{ textDecoration: "none", color: "white" }}>
             <Typography
               variant="h6"
               noWrap
               component="a"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
+              className={styles.appTitle}
             >
               mplWEB
             </Typography>
           </Link>
 
-          {/* mobile */}
-          {/* <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
-          {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            mplWEB
-          </Typography> */}
-          {/* desktop */}
-          {/* <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box> */}
-
           {isLoggedIn && (
-            <Box sx={{ flexGrow: 1, textAlign: "end" }}>
+            <Box className={styles.more}>
               <Tooltip title="More">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <IconButton
+                  onClick={handleOpenUserMenu}
+                  className={styles.iconButton}
+                >
                   <Avatar />
                 </IconButton>
               </Tooltip>
               <Menu
-                sx={{ mt: "45px" }}
+                className={styles.menu}
                 id="menu-appbar"
                 anchorEl={anchorElUser}
                 anchorOrigin={{
@@ -171,9 +88,7 @@ const Navbar = (props) => {
                   </Link>
                 </MenuItem>
                 <MenuItem key={"logout"} onClick={handleLogout}>
-                  <Typography sx={{ textAlign: "center", color: "black" }}>
-                    Logout
-                  </Typography>
+                  <Typography className={styles.logout}>Logout</Typography>
                 </MenuItem>
               </Menu>
             </Box>
