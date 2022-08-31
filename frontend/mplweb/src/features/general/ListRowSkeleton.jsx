4fdf6d04f12dbddafec0045209411bd5302e0376
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2022-08-31 19:45:48 +0200

    Add a min-height to FeedbackList and DemosList, adjust ListRowSkeleton's styling

--- frontend/mplweb/src/features/general/ListRowSkeleton.jsx
+++ frontend/mplweb/src/features/general/ListRowSkeleton.jsx
@@ -2,12 +2,14 @@ import React, { Fragment } from "react";
 import { ListItem, Divider } from "@mui/material";
 import { Skeleton } from "@mui/material";
 
+import styles from "./ListRowSkeleton.module.css";
+
 const createSkeleton = (rows, styling) => {
   let res = [];
   for (let i = 0; i < rows; i++) {
     res.push(
       <Fragment key={i}>
-        <ListItem button className={styling} id={i}>
+        <ListItem button className={styles.item} id={i}>
           <Skeleton animation="wave" variant="circle" width={40} height={40} />
           <Skeleton animation="wave" variant="rect" width={"95%"} height={40} />
         </ListItem>
