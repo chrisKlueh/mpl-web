6ed5b33d503e3da0fc1e94086174d2c630bd1771
chrisKlueh <chrisKlueh@christian-klueh@outlook.de>
2022-09-23 17:03:57 +0200

    Fix secret key variable name

--- backend/mplweb/settings.py
+++ backend/mplweb/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-ve%+@npba-l+p+&hzlm*5&)+$&s&ni&kppth5zi1)3(7rr2fy('
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
