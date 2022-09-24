e8f2567177ea38da8dcc85b9329b00b7da98b8e4
chrisKlueh <chrisKlueh@christian-klueh@outlook.de>
2022-09-24 16:41:30 +0200

    specify correct wsgi file

--- docker/backend/wsgi-entrypoint.sh
+++ docker/backend/wsgi-entrypoint.sh
@@ -13,7 +13,7 @@ done
 
 ./manage.py collectstatic --noinput
 
-gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
+gunicorn mplweb.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
 
 #####################################################################################
 # Options to DEBUG Django server
