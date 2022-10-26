e37cdf1e70db866ab4dca9de5899942db0bafb91
chrisKlueh <christian-klueh@outlook.de>
2022-10-26 20:49:41 +0200

    Add a axios interceptor to refresh token when it is expired (based on a tutorial)

--- frontend/src/api/axiosApi.js
+++ frontend/src/api/axiosApi.js
@@ -10,3 +10,45 @@ export const axiosInstance = axios.create({
     accept: "application/json",
   },
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response.status === 401 &&
+      error.response.statusText === "Unauthorized" &&
+      error.response.data.code === "token_not_valid"
+    ) {
+      const refreshToken = localStorage.getItem("refresh_token");
+      if (refreshToken) {
+        const tokenParts = JSON.parse(atob(refreshToken.split(".")[1]));
+        // exp date in token is expressed in seconds, while now() returns milliseconds:
+        const now = Math.ceil(Date.now() / 1000);
+        if (tokenParts.exp > now) {
+          return axiosInstance
+            .post("/token/refresh/", { refresh: refreshToken })
+            .then((response) => {
+              localStorage.setItem("access_token", response.data.access);
+              localStorage.setItem("refresh_token", response.data.refresh);
+
+              axiosInstance.defaults.headers["Authorization"] =
+                "JWT " + response.data.access;
+              originalRequest.headers["Authorization"] =
+                "JWT " + response.data.access;
+
+              return axiosInstance(originalRequest);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else {
+          console.log("Refresh token is expired", tokenParts.exp, now);
+        }
+      } else {
+        console.log("no refresh token");
+      }
+    } // else error is different from 401
+    return Promise.reject(error);
+  }
+);
