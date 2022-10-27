b741dc6d54c0af888fe12c882809079cd95dfc98
chrisKlueh <christian-klueh@outlook.de>
2022-10-27 18:20:56 +0200

    Decrease lifetime of refresh tokens, improve Logout(...)View

--- backend/api/views.py
+++ backend/api/views.py
@@ -48,8 +48,7 @@ class LogoutAndBlacklistRefreshTokenView(APIView):
     def post(self, request):
         try:
             refresh_token = request.data["refresh_token"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()
+            RefreshToken(refresh_token).blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
--- backend/mplweb/settings.py
+++ backend/mplweb/settings.py
@@ -63,7 +63,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
+    'REFRESH_TOKEN_LIFETIME': timedelta(hours=12),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
