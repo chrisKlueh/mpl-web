5c2ea9925a548dfcd857b1e51e0fcb05a86cf992
chrisKlueh <christian-klueh@outlook.de>
2022-12-22 22:06:21 +0100

    remotePlotStream: remove unnecessary args

--- backend/api/remotePlotStream.py
+++ backend/api/remotePlotStream.py
@@ -1,9 +1,5 @@
-# Hosts a web server, sends the camera's video to it
-# via WebRTC, receives captured input from the browser via 
-# WebRTC and forwards these inputs to the running demo 
-# instance via matplotlib events.
-
-# Based on aiortc/examples/webcam/webcam.py; modified
+# Based on aiortc/examples/webcam/webcam.py; 
+# heavily modified
 
 import argparse
 import asyncio
@@ -147,15 +143,6 @@ class RemotePlotStream(object):
         # subscribe to the draw event
         self.cid = self.demoFig.canvas.mpl_connect('draw_event', self.onUpdatePlot)
 
-    def force_codec(self, pc, sender, forced_codec):
-        kind = forced_codec.split("/")[0]
-        codecs = RTCRtpSender.getCapabilities(kind).codecs
-        print(codecs)
-        transceiver = next(t for t in pc.getTransceivers() if t.sender == sender)
-        transceiver.setCodecPreferences(
-            [codec for codec in codecs if codec.mimeType == forced_codec]
-        )
-
     async def offer(self, message):
         offer = RTCSessionDescription(sdp=message["data"]["sdp"], type=message["data"]["type"])
         config = RTCConfiguration(ICE_SERVERS)
@@ -200,12 +187,6 @@ class RemotePlotStream(object):
         self.imageRenderingTrack = ImageRenderingTrack()
         print(f"\nRecording plot with {self.plotWidth}x{self.plotHeight}@{FRAMERATE}fps\n")
         video_sender = pc.addTrack(self.imageRenderingTrack)
-        
-        if args.video_codec:
-            self.force_codec(pc, video_sender, args.video_codec)
-        elif args.play_without_decoding:
-            raise Exception(
-                "You must specify the video codec using --video-codec")
 
         await pc.setRemoteDescription(offer)
         answer = await pc.createAnswer()
@@ -302,31 +283,7 @@ class RemotePlotStream(object):
 
 def add_args():
     parser = argparse.ArgumentParser(description="WebRTC webcam demo")
-    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
-    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
-    parser.add_argument(
-        "--play-from", help="Read the media from a file and sent it."),
-    parser.add_argument(
-        "--play-without-decoding",
-        help=(
-            "Read the media without decoding it (experimental). "
-            "For now it only works with an MPEGTS container with only H.264 video."
-        ),
-        action="store_true",
-    )
-    parser.add_argument(
-        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
-    )
-    parser.add_argument(
-        "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
-    )
     parser.add_argument("--verbose", "-v", action="count")
-    parser.add_argument(
-        "--audio-codec", help="Force a specific audio codec (e.g. audio/opus)"
-    )
-    parser.add_argument(
-        "--video-codec", help="Force a specific video codec (e.g. video/H264)"
-    )
     parser.add_argument("--demo", help="The filename of the demo (without .py)", required=True)
     parser.add_argument("--instance_id", help="The instance's id", required=True)
     parser.add_argument("--group_id", help="The instance owner's group", required=True)
@@ -342,13 +299,7 @@ if __name__ == "__main__":
     else:
         logging.basicConfig(level=logging.INFO)
 
-    if args.cert_file:
-        ssl_context = ssl.SSLContext()
-        ssl_context.load_cert_chain(args.cert_file, args.key_file)
-    else:
-        ssl_context = None
-
-        demoModule = import_module("." + args.demo, "demo_files")
-        demo = demoModule.Demo(blocking=False)
-        
-        remotePlotStream = RemotePlotStream(demo, args.instance_id, args.group_id, args.refresh_token)
+    demoModule = import_module("." + args.demo, "demo_files")
+    demo = demoModule.Demo(blocking=False)
+    
+    remotePlotStream = RemotePlotStream(demo, args.instance_id, args.group_id, args.refresh_token)
